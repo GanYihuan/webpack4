583e16149a1fc550b7e798f9a924ff4bf0caa043
GanYihuan <583520052@qq.com>
2019-03-14 09:24:57 +0800

    03

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+﻿let path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // 入口
+  output: {
+    filename: 'bundle.js', // 打包后文件名
+    path: path.resolve(__dirname, 'build') // 打包后文件放哪里, 路径必须是一个绝对路径, path.resolve 相对路径解析成绝对路径
+  }
+}
