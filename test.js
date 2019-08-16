f499ae786cf743e661d0c59a14accbc296d5f9bf
GanYihuan <583520052@qq.com>
2019-08-16 15:10:24 +0800

    f

--- test.js
+++ test.js
@@ -1,64 +1,82 @@
-/*
- * @Description:
- * @version:
- * @Author: GanEhank
- * @Date: 2019-04-12 22:38:24
- * @LastEditors: GanEhank
- * @LastEditTime: 2019-08-14 22:21:31
- */
 const webpack = require('webpack')
+const webpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MinicssExtracPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const MinicssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizecssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const WebpackBundleAnaylzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const Happypack = require('happypack')
 
 module.exports = {
   mode: 'production',
   entry: {
-    index: '',
-    other: ''
+    index: ''
   },
   output: {
-    filename: '',
-    chunkFilename: '',
+    filename: '[name].[hash:5].js',
+    chunkFilename: '[name].[contenthash].js',
     publicPath: '',
     path: path.resolve(__dirname, 'dist'),
     library: '',
-    libraryTarget: ''
+    libraryTarget: 'umd'
   },
   optimization: {
     runtimeChunk: {
       name: 'runtime'
     },
-    usedExports: true,
     minimizer: [
       new UglifyJsWebpackPlugin({
         sourceMap: true,
         cache: true,
         parallel: true
       }),
-      new OptimizeCssAssetsWebpackPlugin({})
-    ]
+      new OptimizecssAssetsWebpackPlugin({})
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsynscRequests: 5,
+      maxInitialRequests: 3,
+      automaticDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 2,
+          filename: '',
+          priority: 1,
+          test: /[\\/]node_modules[\\/]/
+        },
+        default: {
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 2,
+          filename: '',
+          priority: -1,
+          reuseExistingChunk: true
+        }
+      }
+    }
   },
   watch: true,
   watchOptions: {
-    poll: 1000,
-    aggregateTimeout: 500,
+    poll: 3000,
+    aggregateTimeout: 300,
     ignored: /node_modules/
   },
   resolve: {
     modules: [path.resolve('node_modules')],
-    extensions: ['.js'],
-    mainFields: ['style'],
+    extensions: ['.js', '.vue'],
+    mainFields: ['style', 'main'],
     alias: {
       bootstrap: ''
     }
   },
-  devtool: 'chap-module-source-map',
+  devtool: 'cheap-module-source-map',
   devServer: {
     port: 8080,
     inline: false,
@@ -71,127 +89,37 @@ module.exports = {
     https: true,
     overlay: true,
     lazy: true,
-    contentbase: './build',
+    contentBase: './build',
     historyApiFallback: {
-      htmlAceeptHeaders: ['test/html'],
+      htmlAcceptHeaders: ['text/html'],
       rewrites: [
         {
           from: /\.*/,
-          to: './index.html'
+          to: ''
         }
-      ]
-    },
-    proxy: {
+      ],
+      proxy: {
+        '/react/api': {
+          target: '',
+          changeOrigin: true,
+          headers: {
+            Cookie: ''
+          },
+          logLevel: 'debug',
+          pathRewrite: {
+            '': ''
+          }
+        }
+      },
+      before(app) {
+        app.get('/user', (req, res) => {
+          res.json({name: ''})
+        })
+      }
     }
   },
   externals: {
     jquery: '$'
-  },
-  module: {
-    noParse: /jquery/,
-    rules: [
-      {
-        test: require.resolve('jquery'),
-        use: 'expose-laoder?$'
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-withimg-loader'
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MinicssExtracPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader'
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              sourceMap: true,
-              singleton: true,
-              modules: true,
-              insertAt: 'top',
-              insertInto: '#app',
-              transform: ''
-            }
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              importLoaders: 2
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        use: 'Happypack/loader?id=js',
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name]-[hash:5].[ext]',
-              limit: 2048,
-              publicPath: '',
-              outputPath: 'dist/',
-              useRelativePath: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(eot|woff2?|ttf|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name]-[hash:5].[ext]',
-              limit: 5000,
-              outputPath: ''
-            }
-          }
-        ],
-        {
-          loader: 'img-loader',
-          options: {
-            pngquant: {
-              quality: 80
-            }
-          }
-        }
-      }
-    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -203,54 +131,26 @@ module.exports = {
       },
       hash: true
     }),
-    new MinicssExtracPlugin({
+    new MinicssExtractPlugin({
       filename: '',
-      chunkFilename: ''
+      chunkFilename: '[name].chunk.css'
     }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
+    new webpackBundleAnalyzer(),
+    new CopyWebpackPlugin(),
+    new CleanWebpackPlugin([
       {
-        from: './doc',
-        to: './'
+        from: '',
+        to: ''
       }
     ]),
-    new WebpackBundleAnaylzer(),
-    new Happypack({
-      id: 'js',
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { useBuiltIns: 'usage' }]
-            ],
-            plugins: [
-              ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-              ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-              ['@babel/plugin-transform-runtime'],
-              ['@babel/plugin-syntax-dynamic-import']
-            ]
-          }
-        },
-        {
-          loader: 'imports-loader?this=>window'
-        },
-        {
-          loader: 'eslint-loader',
-          options: {
-            formatter: require('eslint-friendly-formatter')
-          },
-          enforece: 'pre'
-        }
-      ]
-    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NameModulesPlugin(),
     new webpack.NameChunksPlugin(),
-    new webpack.BundlePlugin(''),
+    new webpack.BunnerPlugin(''),
     new webpack.DefinePlugin({
       DEV: JSON.stringfy('')
     }),
+    new webpack.IgnorePlugin(/\.\/locale/, /moment/),
     new webpack.ProvidePlugin({
       $: 'jquery'
     })
