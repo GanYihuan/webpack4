a3898bcd2c5771e5a6d1dba8b19be35fc1d0fef6
GanYihuan <583520052@qq.com>
2019-08-29 03:51:34 +0800

    test

--- test.js
+++ test.js
@@ -3,9 +3,131 @@
  * @Author: GanEhank
  * @Date: 2019-08-26 18:50:49
  * @LastEditors: GanEhank
- * @LastEditTime: 2019-08-27 17:07:57
+ * @LastEditTime: 2019-08-29 03:51:29
  */
+const webpack = require('webpack')
+const WebpackBundleAnalyzerPlugin = require('webpack.bundle.analyzer').WebpackBundleAnalyzerPlugin
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Happypack = require('happypack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = {
+  mode: 'production',
+  entry: {
+    index: ''
+  },
+  output: {
+    filename: '[name].[hash:5].js',
+    chunkFilename: '[name].[contenthash].js',
+    publicPath: '',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'MyLibrary',
+    libraryTarget: 'umd'
+  },
+  optimization: {
+    runtimeChunk: {
+      name: 'runtime'
+    },
+    usedExports: true,
+    minimizer: [
+      new MiniCssExtractPlugin(),
+      new UglifyJsWebpackPlugin({
+        sourceMap: true,
+        cache: true,
+        parallel: true
+      })
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          priority: 1,
+          filename: 'vendors.js',
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 2,
+          test: /[\\/]node_modules[\\/]/
+        },
+        default: {
+          priority: -1,
+          filename: 'common.js',
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 2,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+  watch: true,
+  watchOptions: {
+    poll: 1000,
+    aggregateTimeout: 500,
+    ignored: /node_modules/
+  },
+  devServer: {
+    port: 8080,
+    lazy: true,
+    https: true,
+    inline: false,
+    overlay: true,
+    progress: true,
+    compress: true,
+    hot: true,
+    hotOnly: true,
+    open: true,
+    openPage: '',
+    contentBase: './build',
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /\.*/,
+          to: '/404.html'
+        }
+      ]
+    },
+    proxy: {
+      '/react/api': {
+        target: '',
+        changeOrigin: true,
+        headers: {
+          Cookies: ''
+        },
+        logLevel: 'debug',
+        pathRewrite: {
+          'header.json': 'demo.json'
+        }
+      }
+    },
+    before(app) {
+      app.get('/user', (req, res) => {
+        res.json({ name: '' })
+      })
+    }
+  },
+  resolve: {
+    modules: [path.resolve('node_modules')],
+    extensions: ['.js'],
+    mainFileds: ['style'],
+    alias: {
+      bootstrap: ''
+    }
+  },
+  devtool: 'cheap-module-source-map',
+  externals: {
+    jquery: '$'
+  },
   module: {
     noParse: /jquery/,
     rules: [
@@ -83,7 +205,7 @@ module.exports = {
               name: '[name]-[hash:5].[ext]',
               limit: 2048,
               publicPath: '',
-              outputPath: '',
+              outputPath: 'dist/',
               useRelativePath: true
             }
           },
@@ -121,6 +243,27 @@ module.exports = {
     ]
   },
   plugins: [
+    new WebpackBundleAnalyzerPlugin(),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: '',
+        to: ''
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: '',
+      filename: 'index.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true
+      },
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.css',
+      chunkFilename: '[name].chunk.css'
+    }),
     new Happypack({
       id: 'js',
       use: [
@@ -128,10 +271,10 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env']
+              ['@babel/preset-env', { useBuiltIns: 'useage' }],
             ],
             plugins: [
-              []
+              ['@babel/plugin-proposal-decorators']
             ]
           }
         },
@@ -146,6 +289,20 @@ module.exports = {
           enforce: 'pre'
         }
       ]
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NameModulesPlugin(),
+    new webpack.NameChunksPlugin(),
+    new webpack.BannerPlugin(''),
+    new webpack.DefinePlugin({
+      FLAG: 'true'
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\locale$/,
+      contextRegExp: /moment$/
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery'
     })
   ]
 }
